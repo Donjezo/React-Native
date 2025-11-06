664eb5d2e608210692d3c56e527a20ed003c5dde
Donjetë Zogaj <70452463+Donjezo@users.noreply.github.com>
2025-11-06 20:44:53 +0100

    lesson 2 with class

--- lesson 2/my-app/App.js
+++ lesson 2/my-app/App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import MainScreen from "./components/MainScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './components/MainScreen';
+import MainScreen2 from './components/mainScreen2';
 
 export default function App() {
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <MainScreen />
+      <MainScreen></MainScreen>
+      <MainScreen2></MainScreen2>
     </View>
   );
 }
@@ -15,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
