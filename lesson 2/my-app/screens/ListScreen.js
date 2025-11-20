0762dcc507db285cab74f1569715358d1f2de8b3
Donjetë Zogaj <70452463+Donjezo@users.noreply.github.com>
2025-11-20 20:50:57 +0100

    leeson 6

--- lesson 2/my-app/screens/ListScreen.js
+++ lesson 2/my-app/screens/ListScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, FlatList } from "react-native";
+import { Text, StyleSheet, View, FlatList , Button,TouchableOpacity} from "react-native";
 
 const students = [
   { name: "Eden", surname: "Rocha", age: "17" },
@@ -9,9 +9,18 @@ const students = [
 ];
 
 const ListScreen = () => {
+  let count=0;
   return (
     <View>
-      <Text style={styles.textStyle}>List of Students</Text>
+      <Button 
+         title="Click me"
+         color="purple"
+         onPress={()=>console.log("butoni eshte klikuar",count++)}
+  
+         >
+
+      </Button>
+      <Text style={styles.textStyle}> </Text>
       <FlatList
         data={students}
         keyExtractor={(item) => item.name}
@@ -21,6 +30,19 @@ const ListScreen = () => {
           </Text>
         )}
       />
+
+
+      <TouchableOpacity
+        style={styles.touchableBtn}
+        onPress={()=>console.log("butoni eshte klikuar nga toucheble opacity",count++)}
+      
+      >
+        <Text style={styles.btnText}>click here</Text>
+
+      </TouchableOpacity>
+
+
+    
     </View>
   );
 };
@@ -30,6 +52,21 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginVertical: 5,
   },
+  btnText:{
+    color:"white",
+    textAlign:"center",
+    fontSize:25,
+    fontWeight:"bold"
+
+  },
+  touchableBtn:{
+    backgroundColor:"purple",
+    marginVertical:15,
+    paddingVertical:20,
+    borderRadius:6,
+    marginHorizontal:20
+
+  }
 });
 
 export default ListScreen;
