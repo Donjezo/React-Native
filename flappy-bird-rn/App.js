24a1be1349fa84e99373f8711be9e162d13548cb
Donjetë Zogaj <70452463+Donjezo@users.noreply.github.com>
2026-02-24 14:57:05 +0100

    lesson 26

---
+++ flappy-bird-rn/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Bird from './src/components/Bird';
+
+export default function App() {
+
+  const screenWidth = Dimensions.get("screen").width;
+  const screenHeight = Dimensions.get("screen").height;
+
+  const birdLeft = screenWidth / 2;
+  const [birdBottom, setBirdBottom] = useState(screenHeight / 2);
+
+  const gravity = 3;
+  let gameTimerId;
+
+  useEffect(() => {
+    if (birdBottom > 0) {
+      gameTimerId = setInterval(() => {
+        setBirdBottom((birdBottom) => birdBottom - gravity);
+      }, 30);
+    }
+
+    return () => {
+      clearInterval(gameTimerId);
+    };
+  }, [birdBottom]);
+
+  return (
+    <View style={styles.container}>
+      <Bird 
+        birdBottom={birdBottom}
+        birdLeft={birdLeft}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
